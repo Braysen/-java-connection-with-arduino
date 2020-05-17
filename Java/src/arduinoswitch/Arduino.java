c57c536b9d8ddf20bab73b3c2d7098addc9d5fd0
Brayssen <btorrejoncerna@gmail.com>
2020-05-17 15:18:02 -0500

    version 1

---
+++ Java/src/arduinoswitch/Arduino.java
@@ -0,0 +1,13 @@
+package arduinoswitch;
+
+public class Arduino {
+
+    //Se crea una instancia del JFrame para que la interfaz gráfica
+    public static void main(String[] args) {
+        View view = new View();
+
+        view.setVisible(true);
+
+    }
+
+}
